4a900dc38961737dbf21528b2fb930a1af1b1f6c
Robert Katzki <robert@bildungsweb.net>
2012-03-29 14:48:23 +0200

    Changed Mail adress.

--- ViewHelpers/Calculate/MultiplicationViewHelper.php
+++ ViewHelpers/Calculate/MultiplicationViewHelper.php
@@ -3,7 +3,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2011 Robert Katzki <robert@katzki.de>
+ *  (c) 2011 Robert Katzki <robert@bildungsweb.net>
  *  
  *  All rights reserved
  *
